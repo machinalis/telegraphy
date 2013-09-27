c7c2bfaa44917cac708db1e6d2553d117a6aa889
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-09-27 19:55:08 -0300

    Redirect to authentication test by default

--- demo_project/demo_project/urls.py
+++ demo_project/demo_project/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 
 urlpatterns = patterns('',
                        url(r'^demo/',
                        include('apps.telegraphy_demo.urls',
                        namespace='telegraphy_demo')),
+                        (r'^$', RedirectView.as_view(url='/demo/')),
+
 )
