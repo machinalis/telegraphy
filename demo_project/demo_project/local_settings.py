bbdce725a4dfb8a53e4ffebbd94664eb905a08a2
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-10-03 17:39:00 -0300

    Change settings from dict to constants

--- demo_project/demo_project/local_settings.py
+++ demo_project/demo_project/local_settings.py
@@ -1,5 +1,6 @@
-TELEGRAPHY_CONF = {
-    'ENGINE': 'telegraphy.gateway.txwamp.TxWAMPGateway',
-    'URL': 'ws://localhost:9000',
-    'RPC_URL': 'http://localhost:4000/',
-}
+TELEGRAPHY_ENGINE = 'telegraphy.gateway.txwamp.TxWAMPGateway'
+TELEGRAPHY_WS_HOST = None
+TELEGRAPHY_WS_PORT = 9000
+TELEGRAPHY_WS_URI = None
+TELEGRAPHY_RPC_URL = 'http://localhost:4000/'
+
