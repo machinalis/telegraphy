76da0fdf30c57c93a036dd435d8b009153e548b3
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-10-03 18:32:50 -0300

    Template syntax processors

--- demo_project/demo_project/settings.py
+++ demo_project/demo_project/settings.py
@@ -161,7 +161,20 @@ LOGGING = {
     }
 }
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+     "django.core.context_processors.request", # <- HERE
+     "django.contrib.auth.context_processors.auth",
+     "django.core.context_processors.debug",
+     "django.core.context_processors.i18n",
+     "django.core.context_processors.media",
+     "django.core.context_processors.static",
+     "django.core.context_processors.tz",
+     "django.contrib.messages.context_processors.messages",
+)
+
+
 try:
     from local_settings import *
 except ImportError, e:
     pass
+
