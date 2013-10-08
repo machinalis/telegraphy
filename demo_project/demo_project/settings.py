7e542ef45f97f3f0574bce9bab2fbf926a10954e
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-10-07 23:04:37 -0300

    Change path

--- demo_project/demo_project/settings.py
+++ demo_project/demo_project/settings.py
@@ -126,7 +126,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'django_telegraphy',
+    'telegraphy.contrib.django.app',
     'apps.telegraphy_demo',
 )
 
