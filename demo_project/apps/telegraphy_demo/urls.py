35f954bf9325ef2c45a3f23e2397a5353a337d3e
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-12-03 13:25:36 -0300

    Reorganize views

--- demo_project/apps/telegraphy_demo/urls.py
+++ demo_project/apps/telegraphy_demo/urls.py
@@ -1,4 +1,12 @@
 from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
 
 
-urlpatterns = patterns('', url(r'^$', 'apps.telegraphy_demo.views.home', name='home'),)
+urlpatterns = patterns('',
+    url(r'^autobahn$',
+        TemplateView.as_view(template_name="telegraphy_demo/autobahn.html"),
+        name='home'),
+    url(r'^simple/',
+        TemplateView.as_view(template_name="telegraphy_demo/simple.html"),
+        name='simple'),
+)
--- demo_project/apps/telegraphy_demo/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic.base import TemplateView
-
-
-class HomePageView(TemplateView):
-    template_name = "telegraphy_demo/index.html"
-
-home = HomePageView.as_view()
