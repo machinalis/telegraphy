2cdfd6c8b7a240a9cb856a396abba9794a326c41
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2013-12-03 14:52:17 -0300

    Call autodiscover in the urls file

--- demo_project/demo_project/urls.py
+++ demo_project/demo_project/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView
 
+from telegraphy.contrib.django_telegraphy import events
+
+
+events.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^demo/',
                        include('apps.telegraphy_demo.urls',
                        namespace='telegraphy_demo')),
-                        (r'^$', RedirectView.as_view(url='/demo/')),
-
-)
+                       (r'^$', RedirectView.as_view(url='/demo/')),
+                       )
