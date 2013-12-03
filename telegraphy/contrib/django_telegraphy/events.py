4f68cd510233c1815649b24c9a6a7c7250a6e81d
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2013-12-03 13:23:32 -0300

    Fixed register and implemented autodiscover.

--- telegraphy/contrib/django_telegraphy/events.py
+++ telegraphy/contrib/django_telegraphy/events.py
@@ -1,14 +1,32 @@
 """Real-time, Model-based events"""
 
+import importlib
+import inspect
 import json
 import xmlrpclib
 
+from telegraphy.contrib.django_telegraphy import settings
 from django.core import serializers
 from django.db.models.signals import post_save, post_delete
 
 
-class BaseModelEvent(object):
-    # Default Events-generating operations
+# This list keeps a possibly out-of-data registry of registered events.
+# TODO: a method to update the list through the Gateway
+_events = []
+
+
+def get_registered_events():
+    """Return the list of currently registered events."""
+    # TODO: add an optional parameter to update the list before returning.
+    # TODO: Such update shall be through the Gateway.
+    return _events
+
+
+class BaseEventModel(object):
+    """
+    Base class for events-generating Models.
+
+    """
     OP_CREATE = 'create'
     OP_UPDATE = 'update'
     OP_DELETE = 'delete'
@@ -21,12 +39,18 @@ class BaseModelEvent(object):
 
     def __init__(self):
         if not self.name:
-            self.name = 'apps.MyModel'
+            self.name = self.get_default_name()
+        gateway_proxy_url = settings.TELEGRAPHY_RPC_PARAMS['url']
+        self.gateway_proxy = xmlrpclib.Server(gateway_proxy_url)
 
-        self.gateway_proxy = xmlrpclib.Server('http://localhost:4000/')
+    def get_default_name(self):
+        model = self.get_target_model()
+        module = model.__module__.split('.')[-2]  # Miss the prefix and .models
+        return '.'.join([module, model.__name__])
+
 
     def get_target_model(self):
-        if type(self.__class__) == BaseModelEvent:
+        if type(self.__class__) == BaseEventModel:
             raise NotImplementedError("Use a specific Event class")
         if isinstance(self.model, str):
             raise NotImplementedError(
@@ -48,13 +72,20 @@ class BaseModelEvent(object):
         """
         Connect to the Django signals following the configured operations.
         """
+        global _event
+
         sender = self.get_target_model()
+
         if self.OP_CREATE in self.operations:
             post_save.connect(self.on_model_create, sender=sender)
+
         if self.OP_UPDATE in self.operations:
             post_save.connect(self.on_model_update, sender=sender)
+
         if self.OP_DELETE in self.operations:
             post_delete.connect(self.on_model_delete, sender=sender)
+
+        _events.append(self)
 
     def send_to_gateway(self, instance, event_type):
         """
@@ -74,3 +105,32 @@ class BaseModelEvent(object):
     def serialize_event_data(self, instance):
         """Return a JSON representation of the model instance's fields."""
         return serializers.serialize("json", [instance], fields=self.fields)
+
+
+def autodiscover():
+    """
+    For all the installed apps in the Django project, register all the
+    BaseEventModel subclasses. They are looked for in the 'events.py' files.
+
+    """
+    if not _events:
+        events_module_name = settings.EVENTS_MODULE_NAME
+
+        def is_event_model(module):
+            return (inspect.isclass(module) and
+                    issubclass(module, BaseEventModel) and
+                    module != BaseEventModel)
+
+        for app in settings.django_settings.INSTALLED_APPS:
+            events_module = None
+            try:
+                app_events_module = '%s.%s' % (app, events_module_name)
+                events_module = importlib.import_module(app_events_module)
+            except ImportError:
+                # No events_module in this app.
+                pass
+            event_classes = inspect.getmembers(events_module, is_event_model)
+            for cname, EventClass in event_classes:
+                print "Event classes detected in app %s: %s" % (app, cname)
+                event = EventClass()
+                event.register()
--- telegraphy/contrib/django_telegraphy/tests.py
+++ telegraphy/contrib/django_telegraphy/tests.py
@@ -3,15 +3,19 @@ from mock import patch, MagicMock
 # from django.db.models.signals import post_save, post_delete
 from django.utils.unittest import TestCase
 
-from django.dispatch import Signal
-from events import BaseModelEvent
+from events import BaseEventModel, post_save, post_delete
 
 
 class BaseModelEventSendToGatewayTests(TestCase):
     def setUp(self):
-        self.model = BaseModelEvent()
-        self.model.name = 'TestEventModel'
-        self.model.gateway_proxy = MagicMock()
+
+        patcher = patch.object(BaseEventModel, 'get_default_name')
+        patcher.start()
+        self.addCleanup(patcher.stop)
+
+        self.event = BaseEventModel()
+        self.event.name = 'TestEventModel'
+        self.event.gateway_proxy = MagicMock()
         self.instance = MagicMock()
         self.instance.serialize_event_data = MagicMock()
 
@@ -20,40 +24,43 @@ class BaseModelEventSendToGatewayTests(TestCase):
         self.addCleanup(patcher.stop)
 
     def test_if_instance_has_serialize_event_data_it_is_used(self):
-        self.model.send_to_gateway(self.instance, None)
+        self.event.send_to_gateway(self.instance, None)
         self.instance.serialize_event_data.assert_called_once_with()
 
     def test_instance_dont_have_serialize_event_data_self_method_is_used(self):
         delattr(self.instance, 'serialize_event_data')
-        with patch.object(self.model, 'serialize_event_data') as mock_serialize:
-            self.model.send_to_gateway(self.instance, None)
+        with patch.object(self.event, 'serialize_event_data') as mock_serialize:
+            self.event.send_to_gateway(self.instance, None)
             mock_serialize.assert_called_once_with(self.instance)
 
     def test_json_dumps_is_called_the_correct_event_data_as_a_dict(self):
         event_type = 'test event type'
-        self.model.send_to_gateway(self.instance, event_type)
+        self.event.send_to_gateway(self.instance, event_type)
         self.mock_json.assert_called_once_with({
-            'name': self.model.name,
+            'name': self.event.name,
             'meta': {'event_type': event_type},
             'data': self.instance.serialize_event_data.return_value})
 
     def test_serialize_event_data_uses_django_serializers(self):
         delattr(self.instance, 'serialize_event_data')
         with patch('django.core.serializers.serialize') as mock_serialize:
-            self.model.send_to_gateway(self.instance, None)
+            self.event.send_to_gateway(self.instance, None)
             mock_serialize.assert_called_once_with(
-                'json', [self.instance], fields=self.model.fields)
+                'json', [self.instance], fields=self.event.fields)
 
     def test_gateway_proxy_send_event_is_called(self):
-        self.model.send_to_gateway(self.instance, None)
-        self.model.gateway_proxy.send_event.assert_called_once_with(
+        self.event.send_to_gateway(self.instance, None)
+        self.event.gateway_proxy.send_event.assert_called_once_with(
             self.mock_json.return_value)
 
 
 class BaseModelEventRegisterTests(TestCase):
 
     def setUp(self):
-        self.event = BaseModelEvent()
+        patcher = patch.object(BaseEventModel, 'get_default_name')
+        patcher.start()
+        self.addCleanup(patcher.stop)
+        self.event = BaseEventModel()
         self.event.operations = ()
         patcher = patch.object(self.event, 'get_target_model')
         self.mock_get_model = patcher.start()
@@ -62,3 +69,11 @@ class BaseModelEventRegisterTests(TestCase):
     def test_get_target_model_is_called(self):
         self.event.register()
         self.mock_get_model.assert_called_once_with()
+
+    def test_post_save_is_connected_to_on_model_create_if_OP_CREATE(self):
+        self.event.operations = (BaseEventModel.OP_CREATE)
+        with patch.object(post_save, 'connect') as m_conn:
+            self.event.register()
+            m_conn.assert_called_once_with(
+                self.event.on_model_create,
+                sender=self.mock_get_model.return_value)
