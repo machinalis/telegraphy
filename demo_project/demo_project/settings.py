eed94cf7de563708d4223a351a0543e17beb484f
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2013-12-03 16:28:04 -0300

    Activate the admin for the demo app

--- demo_project/demo_project/settings.py
+++ demo_project/demo_project/settings.py
@@ -122,10 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
     'telegraphy.contrib.django_telegraphy',
     'apps.telegraphy_demo',
 )
--- demo_project/demo_project/urls.py
+++ demo_project/demo_project/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 from django.views.generic import RedirectView
 
 from telegraphy.contrib.django_telegraphy import events
 
-
+admin.autodiscover()
 events.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,4 +12,5 @@ urlpatterns = patterns('',
                        include('apps.telegraphy_demo.urls',
                        namespace='telegraphy_demo')),
                        (r'^$', RedirectView.as_view(url='/demo/')),
+                        (r'^admin/', include(admin.site.urls)),
                        )
