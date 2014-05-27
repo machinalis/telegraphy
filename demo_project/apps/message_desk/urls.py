3469eff3d1d00db683fcdf3bc3665f7f7e9d7bdf
Joaquín Sorianello <joac@joac.com.ar>
2014-05-27 09:44:06 -0300

    Revert "Message Desk Skeleton"
    This reverts commit 0246d07a291b54e3ea476cf86f21e1724b0c6c59.

--- demo_project/apps/message_desk/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-
-urlpatterns =  patterns('',
-    url("^$", 'apps.message_desk.views.index', name='index'),
-)
--- demo_project/demo_project/urls.py
+++ demo_project/demo_project/urls.py
@@ -7,12 +7,10 @@ from telegraphy.contrib.django_telegraphy import events
 admin.autodiscover()
 events.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    url(r'^demo/', include('apps.telegraphy_demo.urls',
-                           namespace='telegraphy_demo')),
-    (r'^$', RedirectView.as_view(url='/demo/')),
-    url(r'^message-desk/', include('apps.message_desk.urls',
-                                   namespace='message_desk')),
-    (r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('',
+                       url(r'^demo/',
+                       include('apps.telegraphy_demo.urls',
+                       namespace='telegraphy_demo')),
+                       (r'^$', RedirectView.as_view(url='/demo/')),
+                        (r'^admin/', include(admin.site.urls)),
+                       )
