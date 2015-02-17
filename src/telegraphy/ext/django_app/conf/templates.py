84f1f4dbd55d76f79ad216219b66289520f20746
Nahuel Defosse <nahuel.defosse@gmail.com>
2015-02-16 21:23:27 -0300

    Crossbar.IO template and django settiong
    Note that we use the app/conf module for Djanco configuration.
    Take a look at that module until we have documented it thoroughly

---
+++ src/telegraphy/ext/django_app/conf/templates.py
@@ -0,0 +1,65 @@
+# Gracefyllt borrowed from:
+# http://crossbar.io/docs/Adding-Real-Time-to-Django-Applications/
+# Since this is Django specific and most of Telegraphy is about configuring things for
+# you, there are some Tmeplate logic in this template for easy autoconfiguration.
+
+DEFAULT_CONFIG = '''
+{
+   "workers": [
+      {
+         "type": "router",
+         "options": {
+            "pythonpath": {{ PYTHON_PATH }}
+         },
+         "realms": [
+            {
+               "name": "realm1",
+               "roles": [
+                  {
+                     "name": "anonymous",
+                     "permissions": [
+                        {
+                           "uri": "*",
+                           "publish": true,
+                           "subscribe": true,
+                           "call": true,
+                           "register": true
+                        }
+                     ]
+                  }
+               ]
+            }
+         ],
+         "transports": [
+            {
+               "type": "web",
+               "endpoint": {
+                  "type": "tcp",
+                  "port": 8080
+               },
+               "paths": {
+                  "/": {
+                     "type": "wsgi",
+                     "module": "{{ PROJECT_NAME }}.wsgi",
+                     "object": "application"
+                  },
+                  "ws": {
+                     "type": "websocket",
+                     "debug": false
+                  },
+                  "notify": {
+                     "type": "pusher",
+                     "realm": "realm1",
+                     "role": "anonymous"
+                  },
+                  "{{ STATIC_URL }}": {
+                     "type": "static",
+                     "directory": "{{ STATIC_ROOT }}"
+                  }
+               }
+            }
+         ]
+      }
+   ]
+}
+'''
